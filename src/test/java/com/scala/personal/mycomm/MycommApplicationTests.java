f2c2f509d2866c370940a1233ec77344732b0a31
Scala <scala0114@gmail.com>
2022-05-04 22:54:09 +0900

    Initial commit

---
+++ src/test/java/com/scala/personal/mycomm/MycommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scala.personal.mycomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
